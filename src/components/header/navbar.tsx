f6c5b7435f0a94fa54d27723bb246de7834ca114
Lucas Anacleto <lucasanacleto@outlook.com.br>
2022-10-12 20:21:20 -0300

    add menu items

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -9,9 +9,13 @@ import PlanningIcon from "../../../public/icons/icon-planning.svg";
 import UpArrowIcon from "../../../public/icons/icon-arrow-up.svg";
 import DownArrowIcon from "../../../public/icons/icon-arrow-down.svg";
 import Menuitems from "./menu-items";
+import { useState } from "react";
 
 const Navbar = () => {
-  const navItems = {
+  const [menuItem, setMenuItem] = useState(false);
+  const showMenuList = () => {};
+
+  const navItems: INavItems = {
     Features: [
       { icon: TodoIcon, name: "Todo List", link: "#" },
       { icon: CalendarIcon, name: "Calendar", link: "#" },
@@ -27,24 +31,59 @@ const Navbar = () => {
     About: [],
   };
 
-  // console.log(Object.keys(navItems));
+  interface INavItems {
+    [key: string]: {
+      name?: string;
+      icon?: string;
+      link?: string;
+    }[];
+  }
+
+  const obj = {
+    teste: "oi",
+  };
+
+  // const keys = Object.keys
+
+  console.log(navItems.About);
 
   return (
     <nav className="navbar">
-      <img src={Logo} alt="Logo" />
-      {/* {Object.keys(navItems).map((item, index) => (
-        <li key={index}>{navItems[item][]}</li>
-      ))} */}
+      <img className="navbar__logo" src={Logo} alt="Logo" />
       <div className="menu-items-container">
-        <div className="dropdown-item">
+        {Object.keys(navItems).map((item, index) => (
+          <div className="dropdown-item" key={index}>
+            <span className="dropown-item__title">{item}</span>
+            {/* {navItems.key(index) ? (
+              navItems.map((el) => (
+                <ul className="dropdown-item__list">
+                  <li>
+                    <img src={TodoIcon} alt="Todo Icon" />
+                    <span>Todo List</span>
+                  </li>
+                  <li>Calendar</li>
+                  <li>Reminders</li>
+                  <li>Planning</li>
+                </ul>
+              ))
+            ) : (
+              <span>teste</span>
+            )} */}
+          </div>
+        ))}
+
+        {/* <div className="dropdown-item">
           <span className="dropown-item__title">Features</span>
           <ul className="dropdown-item__list">
-            <li>Todo List</li>
+            <li>
+              <img src={TodoIcon} alt="Todo Icon" />
+              <span>Todo List</span>
+            </li>
             <li>Calendar</li>
             <li>Reminders</li>
             <li>Planning</li>
           </ul>
-        </div>
+        </div> */}
       </div>
     </nav>
   );
